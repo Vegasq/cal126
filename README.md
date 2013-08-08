e76d472f43eb82c3d09116aa2263b13808fbec4b
Nikolay Yakovlev <vegasq@gmail.com>
2013-08-08 11:57:50 +0300

    * init

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ cal126
 ======
 
 Yet another JavaScript calendar, without html or css, just array with weeks.
+
+# Usage:
+
+var cal = new Cal126();
+
+cal.set_date(new Date());
+
+var calendar = _self.split_by_weeks()
---
+++ cal126.js
@@ -0,0 +1,89 @@
+function Cal126(){
+    _self = this;
+
+    _self.month = function(){ return _self.working_date.getMonth(); };
+    _self.day = function(){   return _self.working_date.getDate(); };
+    _self.year = function(){  return _self.working_date.getFullYear(); };
+    _self.cal = function(){ return _self.split_by_weeks(); }
+
+    _self.get_month_name = function(){
+        var month_names = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov", "Dec"];
+        return month_names[_self.month()];
+    };
+
+    _self.get_month_offset = function(){
+        // Offset for first week
+        var month = _self.month() + 1;
+        var date = new Date(month + ' 1 ,' + _self.year());
+        return date.getDay();
+    };
+
+    _self.get_month_len = function(){
+        // hardcoded month len and feb ;)
+        var total_feb = 28;
+
+        if (_self.month() == 1){
+            if ( ( _self.year() % 100 != 0) && 
+                 ( _self.year() % 4   == 0) || 
+                 ( _self.year() % 400 == 0)){
+
+                total_feb = 29;
+            }
+        }
+
+        _self.total_days = ["31", "" + total_feb,"31","30","31","30","31","31","30","31","30","31"];
+
+        return _self.total_days[_self.month()];
+    };
+
+    _self.set_date = function(user_date){
+        // Check user input
+        if(user_date == undefined){
+            _self.working_date = new Date();
+        } else if ( user_date instanceof Date ) {
+            _self.working_date = user_date;
+        }
+    };
+
+    _self.split_by_weeks = function(){
+        // Split int array into weeks, 
+        var days = cal.get_month_len()
+        var offset = cal.get_month_offset();
+
+        var week = [];
+        var month = [];
+        var offset_used = false;
+
+        var i = 1;
+        while ( i <= days ){
+            // For first loop we append offset
+            if ( offset_used == false ){
+                while (offset > 0){
+                    week.push(0);
+                    offset_used = true;
+                    offset --;
+                }
+            }
+
+            // Append date to week
+            week.push(i);
+
+            // Last week append
+            if( i == days ){
+                while ( week.length != 7){
+                    week.push(0);
+                }
+            }
+
+            // And check for week len
+            if(week.length == 7){
+                month.push(week);
+                week = [];
+            }
+
+            i++;
+        }
+
+        return month;
+    };
+}
